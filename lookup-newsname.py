f687553e91229f57a8600f1a0acaf2c9f881d41f
tkimu3 <t20468616+tkimu3@users.noreply.github.com>
2023-02-06 10:16:59 +0900

    Create functions

--- lookup-newsname.py
+++ lookup-newsname.py
@@ -1,22 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Define the base url of the website
-BASE_URL = 'https://www2.nhk.or.jp/gogaku/gendaieigo/detail/index.html'
 
-# Input date and create the url to access the website for the date
-date = input("Input date:")
-url = BASE_URL + "?no=" + date
+# Define the base URL and Query Parameter of the website
+BASE_URL = 'https://www2.nhk.or.jp/gogaku/gendaieigo/detail/index.html'
+QUERY_PARAM = '?no='
+
+def create_url_from_date_query_param(base_url, query_param):
+    # Input date and create the url to access the website for the date
+    date = input("Input date:")
+    url = base_url + query_param + date
+    return url
+
 
 # Print the created url of the website
+url = create_url_from_date_query_param(BASE_URL,QUERY_PARAM)
 print(url)
 
-# Get the HTML file from the URL
-html = requests.get(url)
+def getHTMLcontents(url):
+    # Get the HTML file from the URL
+    html = requests.get(url)
+    # Read the HTML file using BeautifulSoup
+    content = BeautifulSoup(html.content, "html.parser")
+    return content
 
-# Read the HTML file using BeautifulSoup
-soup = BeautifulSoup(html.content, "html.parser")
-#print(soup)
+
+soup = getHTMLcontents(url)
 
 # Specify the news title using class property
 # https://codezine.jp/article/detail/12230
